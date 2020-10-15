21c0e4affb05b22f36383a5d1a60749c7c266877
Fernando Barros <fernandob.delima@outlook.com>
2020-10-15 12:36:43 -0300

    Criado a database e configurado a conexão

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/user' , (request, response) => {
+    return response.json({message: 'hello world'});
+});
 
 app.listen(3333);
