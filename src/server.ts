4e17eb75846d9c057bd4be8f85806da3a01e7ec9
Fernando Barros <fernandob.delima@outlook.com>
2020-10-15 12:53:47 -0300

    Criando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
+import express from "express";
+import "./database/connection";
 
-import './database/connection';
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/user' , (request, response) => {
-    return response.json({message: 'hello world'});
-});
+app.use(routes);
 
 app.listen(3333);
